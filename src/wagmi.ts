3f3f79fa87093ee8e2ccd5c8b9bb0acab3fba2b8
Abolare Roheemah <roheemahabo@gmail.com>
2024-09-30 18:11:29 +0100

    fixes error

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,28 +1,4 @@
-import { w3mConnectors, w3mProvider } from "@web3modal/ethereum";
-import { configureChains, createConfig } from "wagmi";
-import { mainnet, scrollTestnet } from "wagmi/chains";
-import { scrollSepolia } from "../src/app/components/customChains/scrollSepolia";
-// import { scrollSepolia } from "viem/src/chains";
+
 require("dotenv").config();
 
 export const walletConnectProjectId = process.env.NEXT_PUBLIC_PROJECT_ID;
-
-// 'npm init wagmi --template next-connectkit'
-
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet, scrollTestnet, scrollSepolia],
-  [w3mProvider({ projectId: walletConnectProjectId })]
-);
-
-export const config = createConfig({
-  autoConnect: true,
-  connectors: w3mConnectors({
-    chains,
-    projectId: walletConnectProjectId,
-    version: 2,
-  }),
-  publicClient,
-  webSocketPublicClient,
-});
-
-export { chains };
